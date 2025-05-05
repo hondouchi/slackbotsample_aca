2e08c4b4820be8134c13f8d6375c40bf72de624e
hondouchi <hondouchi@ap-com.co.jp>
2025-05-05 14:45:35 +0900

    github actions setting

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.message(async ({ message, say }) => {
 
     try {
       await say({
-        text: `メッセージありがとうございます。${replyText}`,
+        text: `メッセージとてもありがとうございます。${replyText}`,
         thread_ts: message.ts,
       });
     } catch (e) {
